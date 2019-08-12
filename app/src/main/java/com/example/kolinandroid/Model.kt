b661e2dd006ac079a8d35b335397597d0edaf4ea
junhyun87 <pe.jeon87@frentree.com>
2019-08-12 17:08:57 +0900

    3-3 Hobby 데이터 삽입 완료

---
+++ app/src/main/java/com/example/kolinandroid/Model.kt
@@ -0,0 +1,38 @@
+package com.example.kolinandroid
+
+data class Hobby(var titls : String)
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Talkibng"),
+        Hobby("Sleeping")
+        )
+}
